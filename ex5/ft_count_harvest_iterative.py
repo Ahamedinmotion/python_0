135e5d0aef8e99b8da84af67650c6db8575a065c
Ahamedinmotion <ahamedinmotion@gmail.com>
2026-02-21 05:46:30 +0400

    Smoll

---
+++ ex5/ft_count_harvest_iterative.py
@@ -0,0 +1,13 @@
+def ft_count_harvest_iterative():
+    while True:
+        try:
+            days = int(input("Days until harvest: "))
+        except ValueError:
+            print("Invalid Value")
+            continue
+        if days < 0:
+            print("Invalid Argument: Negative")
+            continue
+        for i in range(days + 1):
+            print(f"Day {i}")
+        break
